c80ef236b624ff96485cd64b897e1ef361b92a75
GBcatW <118425562+GBcatW@users.noreply.github.com>
2023-03-02 20:34:34 +0700

    update gui

---
+++ sousettia/src/gui/Login.java
@@ -0,0 +1,124 @@
+import java.awt.Color;
+import javax.swing.ImageIcon;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.CardLayout;
+import javax.swing.JLabel;
+import javax.swing.JCheckBox;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+
+public class Login implements ActionListener  {
+
+	private JTextField tfEmail;
+	private JPasswordField passwordField;
+	JFrame frame = new JFrame();
+	JButton btnLogin = new JButton("LOGIN");
+	JButton btnRegister = new JButton("REGISTER");
+	
+	//create a frame
+	public Login() {
+		
+		//logo image
+		ImageIcon image = new ImageIcon("bank.png");
+		frame.setIconImage(image.getImage());
+		
+		//email 
+		JLabel lblEmail = new JLabel("EMAIL");
+		lblEmail.setFont(new Font("Alice", Font.BOLD, 21));
+		lblEmail.setBounds(205 ,311 ,79 ,30);
+		frame.getContentPane().add(lblEmail);
+		
+		//password
+		JLabel lblPassword = new JLabel("PASSWORD");
+		lblPassword.setFont(new Font("Alice", Font.BOLD, 21));
+		lblPassword.setBounds(157, 391, 155, 30);
+		frame.getContentPane().add(lblPassword);
+		
+		//swan image
+		ImageIcon icon = new ImageIcon("137852811086 (1).png");
+		JLabel Icon = new JLabel();
+		Icon.setBounds(5,5,0,0);
+		Icon.setIcon(icon);
+		
+		
+		//panel for image
+		JPanel panel = new JPanel();
+		panel.setBounds(244,31,252,226);
+		panel.add(Icon);
+		frame.getContentPane().add(panel);
+		
+		//loginbtn
+		btnLogin.setFocusable(false);
+		btnLogin.addActionListener(this);
+		btnLogin.setForeground(new Color(255, 132, 153));
+		btnLogin.setBackground(new Color(255, 255, 255));
+		btnLogin.setFont(new Font("Alice", Font.BOLD, 27));
+		btnLogin.setBounds(205, 488, 133, 38);
+		frame.getContentPane().add(btnLogin);
+		
+		//registerbtn
+		btnRegister.setFocusable(false);
+		btnRegister.addActionListener(this);
+		btnRegister.setForeground(new Color(255, 132, 153));
+		btnRegister.setFont(new Font("Alice", Font.BOLD, 27));
+		btnRegister.setBackground(new Color(255, 255, 255));
+		btnRegister.setBounds(404, 488, 191, 38);
+		frame.getContentPane().add(btnRegister);
+		
+		//input email
+		tfEmail = new JTextField();
+		tfEmail.setFont(new Font("Alice", Font.PLAIN, 15));
+		tfEmail.setBackground(new Color(255, 238, 247));
+		tfEmail.setBounds(290, 316, 261, 29);
+		frame.getContentPane().add(tfEmail);
+		tfEmail.setColumns(10);
+		
+		//input password
+		passwordField = new JPasswordField();
+		passwordField.setBackground(new Color(255, 238, 247));
+		passwordField.setBounds(290, 391, 261, 29);
+		passwordField.setColumns(10);
+		frame.getContentPane().add(passwordField);
+		
+		JLabel lblWe = new JLabel("Welcome!",SwingConstants.CENTER);
+		lblWe.setFont(new Font("Alice", Font.BOLD, 17));
+		lblWe.setBounds(169, 449, 394, 30);
+		frame.getContentPane().add(lblWe);
+		
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setBackground(new Color(255, 196, 225));
+		frame.setVisible(true);
+		frame.setBounds(400, 120, 754, 605);
+		frame.setResizable(false);
+		frame.getContentPane().setLayout(null);
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String email;
+		String password;
+		if(e.getSource()==btnRegister) {
+			frame.dispose();
+			Register register = new Register();
+			
+		}
+		
+		if(e.getSource()==btnLogin) {
+			frame.dispose();
+			email = tfEmail.getText();
+			password = passwordField.getText();
+			Homepage homepage = new Homepage();
+		}
+	}
+	
+}
---
+++ sousettia/src/gui/infoRegister.java
@@ -0,0 +1,127 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class infoRegister implements ActionListener {
+
+
+	JFrame frame = new JFrame();
+	JButton btnConfirm = new JButton("Confirm");
+	JButton btnBack = new JButton("Back");
+	private JTextField tfFname;
+	private JTextField tfLname;
+	private JTextField tfID;
+	private JTextField tfAddress;
+	
+	public infoRegister() {
+		ImageIcon image = new ImageIcon("bank.png");
+		frame.setIconImage(image.getImage());
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setBackground(new Color(255, 215, 235));
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setBounds(500, 120, 553, 538);
+		
+		//lblregister
+		JLabel lblRegister = new JLabel("Register");
+		lblRegister.setFont(new Font("Alice", Font.BOLD, 28));
+		lblRegister.setBounds(210, 10, 118, 41);
+		frame.getContentPane().add(lblRegister);
+				
+		//lblFname 
+		JLabel lblFname = new JLabel("Firstname");
+		lblFname.setFont(new Font("Alice", Font.BOLD, 21));
+		lblFname.setBounds(73 ,69 ,118 ,30);
+		frame.getContentPane().add(lblFname);
+				
+		//Input Fname
+		tfFname = new JTextField();
+		tfFname.setBackground(new Color(255, 255, 255));
+		tfFname.setBounds(73, 109, 388, 29);
+		frame.getContentPane().add(tfFname);
+		tfFname.setColumns(10);
+						
+		//lblLname
+		JLabel lblLname = new JLabel("Lastname");
+		lblLname.setFont(new Font("Alice", Font.BOLD, 21));
+		lblLname.setBounds(73, 148, 109, 30);
+		frame.getContentPane().add(lblLname);
+				
+		
+		//Input Lname
+		tfLname = new JTextField();
+		tfLname.setColumns(10);
+		tfLname.setBackground(Color.WHITE);
+		tfLname.setBounds(73, 188, 388, 29);
+		frame.getContentPane().add(tfLname);
+		
+		//lblIDinfo
+		JLabel lblIDinfo = new JLabel("Identification ID");
+		lblIDinfo.setFont(new Font("Alice", Font.BOLD, 21));
+		lblIDinfo.setBounds(73, 222, 180, 30);
+		frame.getContentPane().add(lblIDinfo);
+		
+		//input Identification ID
+		tfID = new JTextField();
+		tfID.setColumns(10);
+		tfID.setBackground(Color.WHITE);
+		tfID.setBounds(73, 262, 388, 29);
+		frame.getContentPane().add(tfID);
+		
+		//lblAddress
+		JLabel lblAddress = new JLabel("Address");
+		lblAddress.setFont(new Font("Alice", Font.BOLD, 21));
+		lblAddress.setBounds(73, 301, 180, 30);
+		frame.getContentPane().add(lblAddress);
+		
+		//input address
+		tfAddress = new JTextField();
+		tfAddress.setColumns(10);
+		tfAddress.setBackground(Color.WHITE);
+		tfAddress.setBounds(73, 341, 388, 29);
+		frame.getContentPane().add(tfAddress);
+		
+		//buttonConfirm
+		btnConfirm.setFocusable(false);
+		btnConfirm.addActionListener(this);
+		btnConfirm.setBackground(Color.WHITE);
+		btnConfirm.setForeground(new Color(255, 132, 153));
+		btnConfirm.setFont(new Font("Alice", Font.BOLD, 25));
+		btnConfirm.setBounds(122, 442, 141, 30);
+		frame.getContentPane().add(btnConfirm);
+				
+		//buttonBack
+		btnBack.setFocusable(false);
+		btnBack.addActionListener(this);
+		btnBack.setForeground(new Color(255, 132, 153));
+		btnBack.setFont(new Font("Alice", Font.BOLD, 25));
+		btnBack.setBackground(Color.WHITE);
+		btnBack.setBounds(291, 442, 118, 30);
+		frame.getContentPane().add(btnBack);
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==btnConfirm) {
+			frame.dispose();
+			Login login = new Login();
+		}
+		if(e.getSource()==btnBack) {
+			frame.dispose();
+			Register register = new Register();
+		}
+	}
+}
